112125f7d8a46afb7652b54df8dc1df2f806072a
Sonu Saini <sonu2311saini@gmail.com>
2022-08-08 02:25:34 +0530

    almost complete

--- src/header.jsx
+++ src/header.jsx
@@ -26,30 +26,13 @@ export function Header2() {
 
 
 
-export function Header() {
-  // const isLogin = ("login_key" in session && "id" in session.login_key && session.login_key.id != null)
-  const profile_name="sonu"
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
-
-
-  const menuOpenClose = function(){
-    setIsMenuOpen(!isMenuOpen)
-    console.log("isMenuOpen===", isMenuOpen)
-  }
-
-  const logout =function(){
-    window.location.href = "#/login"
-  }
-
+export function Header({name}) {
   return (
     <>
       <div className="head_div" >
         <div className="hsplit">
             <div className="logo" >
               <img src="images/logo.png" />
-            </div>
-            <div className="title" style={{display: 'none'}}>
-              Crix
             </div>
             <div className="hsplit navbar_tabs" style={{float: 'right', marginRight: '40px'}}>
               < div className="show-md hide-xs hide-sm heading">
@@ -59,22 +42,8 @@ export function Header() {
                 <img src= "images/profile_image.png"  className='profile_image'/>
               </div>
               <div className="show-md show-xs show-sm heading profile_name">
-                Jeo Stockton
+                {name}
               </div>
-              {/* <div className="show-md hide-xs show-sm heading ">
-                BIBENDUM 
-              </div>
-              <div className="show-md hide-xs show-sm heading">
-                MEGNA
-              </div> */}
-              {/* <div className="hide-lg hide-md show-xs show-sm" style={{paddingTop: '6px', cursor:'pointer'}}>
-                <div className="menu" onClick={menuOpenClose}>
-                  <span className="material-icons" style={{fontWeight: 600}}>
-                    more_vert
-                  </span>
-                </div>
-              </div> */}
-
             </div>
         </div>
       </div>
--- src/home.jsx
+++ src/home.jsx
@@ -3,18 +3,36 @@ import {Header} from './header';
 import React from 'react';
 import {api} from './library';
 
-function Overview(){
-  const l= [1,2,3,4]
+
+function Overview({dashboardStats}){
+  const list = [{
+    text: "Profile Views",
+    value: dashboardStats.profile_views,  
+  },
+  {
+    text: "Mentorship Sessions",
+    value: dashboardStats.mentorship_sessions, 
+  },
+  {
+    text: "Jobs Applied",
+    value: dashboardStats.jobs_applied, 
+  },
+  {
+    text: "Skills Verified",
+    value: dashboardStats.skills_verified
+  },
+  ]
+
   return (
     <>
       <div className='card'>
         <div className='overview_text'>Overview</div>
         <div className='hsplit' style={{boxSizing:'border-box'}}>
-          {l.map((x)=>(
+          {list.map((content)=>(
             <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12' style={{}}>    
               <div className='inside_div_in_card hsplit'>
-                <div className='overview_element_text vheight'>Profile Views  </div>
-                <div className='overview_element_count vheight'>89</div>
+                <div className='overview_element_text vheight'>{content.text}</div>
+                <div className='overview_element_count vheight'>{content.value}</div>
               </div>
             </div>
           ))}         
@@ -24,38 +42,38 @@ function Overview(){
   )
 }
 
-function UpcomingSessions(){
-  const l=[1,2,3,4]
+function UpcomingSessions({upcomingSessions}){
+ 
   return (
     <>
       <div className='card'>
         <div className='overview_text'>Upcoming Sessions</div>
-        {l.map ((x)=>(
+        {upcomingSessions.map ((session)=>(
           <div className='hsplit upcoming_session_element' style={{boxSizing:'border-box'}}>
-            <div className='col-xs-5 hsplit ' style={{}}>
+            <div className='col-xs-6 col-sm-5 col-lg-4 hsplit'>
               <div className='upcoming_session_element_section '>
-                <img src= "images/profile_image.png"  className=''/>
+                <img src= "images/profile2.png"  className='profile_image2'/>
               </div>
-              <div className='upcoming_session_element_section '  style={{marginLeft:"13px"}} >
+              <div className='upcoming_session_element_section' style={{marginLeft:"13px"}} >
                 <div >
-                  <div className='font22 lalign mb5'>Rushil Rai  </div>
-                  <div className='font20 lalign'>Flutter</div>
+                  <div className='font22 lalign mb5'>{session.mentor_name}  </div>
+                  <div className='font15 lalign'>Flutter</div>
                 </div>
               </div> 
             </div>
-            <div className='col-xs-3 upcoming_session_element_section'>
+            <div className='col-xs-2 col-sm-3 col-lg-4 upcoming_session_element_section'>
               <div>
-                <div className='font22 mb5'> 14.00 - 15.00</div>
-                <div className='font20'> 24/10/21</div>
+                <div className='font22 mb5'>{session.timings} </div>
+                <div className='font20'>{session.date}</div>
               </div>  
             </div> 
             <div className=' col-xs-3 upcoming_session_element_section'>
-            <div className='upcoming_session_color_div'> Mentorship</div>
+              <div className='upcoming_session_color_div' style={{backgroundColor:(session.session_type== "Review" ? "rgba(145,174,225,1)": "rgba(232,201,209,1)") }}>{session.session_type}</div>
             </div> 
             <div className=' col-xs-1 upcoming_session_element_section'>
-            <span class="material-icons" style={{fontWeight:"600"}}>
-              arrow_forward
-            </span>
+              <span class="material-icons" style={{fontWeight:"600"}}>
+                arrow_forward
+              </span>
             </div> 
           </div> 
         ))}   
@@ -64,32 +82,121 @@ function UpcomingSessions(){
   )
 }
 
-function RightSection(){
-  
+
+function RightSection({jobPostings}){
+  return (
+    <>
+      <div className='card'>
+        <div className='overview_text'>New Jobs</div>
+        {jobPostings.map ((job)=>(
+          <div className='hsplit upcoming_session_element' style={{boxSizing:'border-box'}}>
+            <div className='col-xs-8 hsplit '>
+              {/* <div className='upcoming_session_element_section'>
+                <img src= "images/profile_image.png"  className=''/>
+              </div> */}
+              <div className='upcoming_session_element_section'style={{marginLeft:"13px"}}>
+                <div >
+                  <div className='font22 lalign mb5'>{job.role} </div>
+                  <div className='font15 lalign'>{job.organization_name}</div>
+                  <div className='font15 lalign'>{job.location}</div>
+                </div>
+              </div> 
+            </div>
+            <div className=' col-xs-4  upcoming_session_element_section '>
+              <div className='font20'>{job.date_posted}</div>
+            </div> 
+          </div> 
+        ))}   
+      </div>
+    </>
+  )
+}
+
+function LeftSEction({shuffle}){
+  return(
+    <>
+      <div className='' style={{marginTop:"20px"}}>
+        <div className="side_panel_element">
+          <div className="hsplit ">
+            <div>
+              <span className="material-icons side_panel_icon">
+                home
+              </span>
+            </div>
+            <div className="side_panel_text" >Home</div>	
+          </div>
+	      </div>
+        <div className="side_panel_element" onClick={shuffle}>
+          <div className="hsplit ">
+            <div>
+              <span className="material-icons side_panel_icon">
+              apps
+              </span>
+            </div>
+            <div className="side_panel_text"  >Shuffle</div>	
+          </div>
+	      </div>
+      </div>
+    </>
+  )
 }
 
 
+function shuffleArray(a) {
+  let array = [...a];
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array;
+}
+
+
+
  export function Home(){
+  
+  const [fullName, setFullName] = React.useState("")
+  const [dashboardStats, setDashboardStats] = React.useState({})
+  const [upcomingSessions, setUpcomingSessions] = React.useState([])
+  const [jobPostings, setJobPostings] = React.useState([])
+  
+  const shuffle = function(){
+    setUpcomingSessions(shuffleArray(upcomingSessions))
+    setJobPostings(shuffleArray(jobPostings))
+     
+  }
+
+  React.useEffect(()=>{
+    fetch("https://mocki.io/v1/bb11aecd-ba61-44b9-9e2c-beabc442d818")
+    .then(x => x.json())
+    .then(function(backend_output){
+      
+        console.log(backend_output);
+
+        setFullName(backend_output.full_name)
+        setDashboardStats(backend_output.dashboard_stats)
+        setUpcomingSessions(backend_output.upcoming_sessions)
+        setJobPostings(backend_output.job_postings)
+      }
+    )
+  }, [])
+
   return (
     <>
-      <Header/>
+      <Header name={fullName}/>
       <div class="hsplit" style={{marginTop:"3px"}}>
-        <div className="left_panel hide-xs hide-sm show-lg hide-md">
-          <div >home</div>
-          <div>Shuffle</div>
+        <div className="col-lg-1 hide-xs hide-sm show-lg hide-md">
+          <LeftSEction  shuffle={shuffle}/>
         </div>
-        <div className='hsplit right_section'>
-          <div className="col-lg-8 col-md-10 col-sm-12 col-xs-12">
-            <Overview/>
-            <UpcomingSessions/>
-          </div>
-          <div className="col-lg-4 col-md-2 col-sm-12 col-xs-12 ">
-            <div className='card'>
-              right card
-            </div>
-          </div>
+        <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12">
+          <Overview dashboardStats={dashboardStats}/>
+          <UpcomingSessions upcomingSessions={upcomingSessions}/>
         </div>
-        
+        <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12 ">
+          <RightSection jobPostings={jobPostings}/>
+        </div> 
       </div>
     </>
   )
